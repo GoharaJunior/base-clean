f3b9ab36890230342e72130402c6d3980963e806
Gohara <gohara.dev@gmail.com>
2023-09-04 21:02:56 -0300

    Adicionado nova estrutura ao arquivo main.js

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,4 +1,21 @@
-/*=== AOS Animation ===*/
-AOS.init({
-    duration: 1000
-});
+(function () {
+    window.theme = {
+        ...window.theme,
+
+        // openApplyOverlayClose: function () {
+
+        // },
+    };
+
+    // Execução de Funções após o carregamento do documento
+    window.addEventListener('DOMContentLoaded', function () {
+        const htmlElement = document.querySelector('html');
+
+        if (htmlElement.classList.contains('page-home')) {
+            /*=== AOS Animation ===*/
+            AOS.init({
+                duration: 1000
+            });
+        }
+    });
+})();
